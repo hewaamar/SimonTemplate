bfc1289433592f3aa3367b15b9ca7fe5507596d5
Hewan Amare <hewaamar557@ed.amdsb.ca>
2022-02-28 15:58:59 -0500

    simon

--- SimonSays/Form1.cs
+++ SimonSays/Form1.cs
@@ -16,14 +16,53 @@ namespace SimonSays
     {
         //TODO: create a List to store the pattern. Must be accessable on other screens
 
+        public static List<int> patternList = new List<int>();
+
+      
+
+
         public Form1()
         {
             InitializeComponent();
+            
         }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //TODO: Launch MenuScreen
+
+            ChangeScreen(this, new MenuScreen());
+
+
+        }
+
+        public static void ChangeScreen(object sender, UserControl next)
+
+        {
+
+            Form f; // will either be the sender or parent of sender 
+
+            if (sender is Form)
+            {
+                f = (Form)sender;
+            }
+
+            else
+            {
+                UserControl current = (UserControl)sender;
+                f = current.FindForm();
+                f.Controls.Remove(current);
+            }
+
+            next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+
+                (f.ClientSize.Height - next.Height) / 2);
+
+
+            f.Controls.Add(next);
+
+            next.Focus();
+
         }
     }
 }
--- SimonSays/GameScreen.cs
+++ SimonSays/GameScreen.cs
@@ -15,7 +15,10 @@ namespace SimonSays
     public partial class GameScreen : UserControl
     {
         //TODO: create guess variable to track what part of the pattern the user is at
+        int guess = 0;
 
+        //buttons
+        Button[] squares = new Button[4];
 
         public GameScreen()
         {
@@ -25,15 +28,94 @@ namespace SimonSays
         private void GameScreen_Load(object sender, EventArgs e)
         {
             //TODO: clear the pattern list from form1, refresh, pause for a bit, and run ComputerTurn()
-        }
+
+            // clear pattern list, refresh, pause
+            Form1.patternList.Clear();
+            Refresh();
+            Thread.Sleep(1000);
+
+
+            //load buttons
+            squares[0] = redButton;
+            squares[1] = yellowButton;
+            squares[2] = blueButton;
+            squares[3] = greenButton;
+
+            // run computer turn
+            ComputerTurn();
+   
+        } 
 
         private void ComputerTurn()
         {
             //TODO: get rand num between 0 and 4 (0, 1, 2, 3) and add to pattern list
+            
+            Random randomNum = new Random();
+            int num = randomNum.Next(0,4);
+            Form1.patternList.Add(0);
+            Form1.patternList.Add(1);
+            Form1.patternList.Add(2);
+            Form1.patternList.Add(3);
 
             //TODO: create a for loop that shows each value in the pattern by lighting up approriate button
+            for (int i = 0; i < Form1.patternList.Count ; i++)
+            {
+                // light up
+                if (Form1.patternList[i] == 0)
+                {
+                    redButton.BackColor = Color.LightPink;
 
-            //TODO: get guess index value back to 0
+                    Refresh();
+                    Thread.Sleep(1000);
+                    redButton.BackColor = Color.DarkRed;
+                    Refresh();
+
+                    Thread.Sleep(300);
+                }
+
+                if (Form1.patternList[i] == 1)
+                {
+                    blueButton.BackColor = Color.LightPink;
+
+                    Refresh();
+                    Thread.Sleep(1000);
+                    blueButton.BackColor = Color.DarkRed;
+                    Refresh();
+
+                    Thread.Sleep(300);
+                }
+
+                if (Form1.patternList[i] == 2)
+                {
+                    yellowButton.BackColor = Color.LightPink;
+
+                    Refresh();
+                    Thread.Sleep(1000);
+                    yellowButton.BackColor = Color.DarkRed;
+                    Refresh();
+
+                    Thread.Sleep(300);
+                }
+
+                if (Form1.patternList[i] == 3)
+                {
+                    greenButton.BackColor = Color.LightPink;
+
+                    Refresh();
+                    Thread.Sleep(1000);
+                    greenButton.BackColor = Color.DarkRed;
+                    Refresh();
+
+                    Thread.Sleep(300);
+                }
+                
+
+
+
+            }
+
+            //TODO: get guess index value back to 0 (tracks number of guesses made by player)
+           
         }
 
         public void GameOver()
@@ -42,18 +124,120 @@ namespace SimonSays
 
             //TODO: close this screen and open the GameOverScreen
 
+            Application.Exit();
         }
 
         //TODO: create one of these event methods for each button
         private void greenButton_Click(object sender, EventArgs e)
         {
             //TODO: is the value at current guess index equal to a green. If so:
+   
+            if (Form1.patternList[guess]==3)
+            {
                 // light up button, play sound, and pause
+                greenButton.BackColor = Color.LightGreen;
+                Thread.Sleep(1000);
                 // set button colour back to original
+                greenButton.BackColor = Color.DarkGreen;
                 // add one to the guess index
-                // check to see if we are at the end of the pattern. If so:
-                    // call ComputerTurn() method
-                // else call GameOver method
+                guess++;
+
+                // call ComputerTurn() method
+                ComputerTurn();
+            }
+
+            else 
+            {
+                GameOver();
+            }
+  
+        }
+
+        private void yellowButton_Click(object sender, EventArgs e)
+        {
+
+
+            if (Form1.patternList[guess] == 1)
+            {
+                // light up button, play sound, and pause
+                greenButton.BackColor = Color.LightGreen;
+                Thread.Sleep(1000);
+                // set button colour back to original
+                greenButton.BackColor = Color.DarkGreen;
+                // add one to the guess index
+                guess++;
+
+                // call ComputerTurn() method
+                ComputerTurn();
+            }
+
+            else
+            {
+                GameOver();
+            }
+
+        }
+
+        private void redButton_Click(object sender, EventArgs e)
+        {
+           
+            if (Form1.patternList[guess] == 0)
+            {
+                // light up button, play sound, and pause
+                greenButton.BackColor = Color.LightGreen;
+                Thread.Sleep(1000);
+                // set button colour back to original
+                greenButton.BackColor = Color.DarkGreen;
+                // add one to the guess index
+                Refresh();
+                // add one to the guess index
+                guess++;
+                // another refresh somewhere
+                ComputerTurn();
+
+               
+            }
+
+            else
+            {
+                GameOver();
+            }
+
+        }
+
+        private void blueButton_Click(object sender, EventArgs e)
+        {
+
+            if (Form1.patternList[guess] == 2)
+            {
+                // light up button, play sound, and pause
+                greenButton.BackColor = Color.LightGreen;
+                //refresh
+                Refresh();
+                Thread.Sleep(1000);
+                // set button colour back to original
+                greenButton.BackColor = Color.DarkGreen;
+                Refresh();
+                // add one to the guess index
+                guess++;
+                // another refresh somewhere
+                ComputerTurn();
+            }
+            else
+            {
+                GameOver();
+            }
+            // if pattern list.  count == guess
+
+            if (Form1.patternList.Count == guess )
+            {
+                ComputerTurn();
+
+
+            }
+
+
+
         }
     }
 }
